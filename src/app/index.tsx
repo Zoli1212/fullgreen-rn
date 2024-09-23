e96e3c8340c7440f60b6db12c99f27c5ff14ac77
butterfly-react <rdigital4455@gmail.com>
2024-09-23 18:44:28 +0200

    0:15

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Hello</Text>
+   
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
