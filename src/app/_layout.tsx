cdee7426919d7f81b7bee77be87c00281c850799
butterfly-react <rdigital4455@gmail.com>
2024-10-31 00:06:09 +0100

    2:30

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,32 +1,35 @@
 import { Stack } from "expo-router";
-import { ToastProvider } from 'react-native-toast-notifications';
+import { ToastProvider } from "react-native-toast-notifications";
+import AuthProvider from "../providers/auth-provider";
 
 export default function RootLayout() {
   return (
     <ToastProvider>
-    <Stack>
-      <Stack.Screen
-        name="(shop)"
-        options={{ title: "Shop",  headerShown: false }}
-      />
-      <Stack.Screen
-        name="categories"
-        options={{ title: "Categories" ,  headerShown: false}}
-      />
-      <Stack.Screen
-        name="product"
-        options={{ title: "Product", headerShown: true }}
-      />
-      <Stack.Screen
-        name="cart"
-        options={{
-          presentation: "modal",
-          title: "Shopping Cart",
-          // Override globally set option
-        }}
-      />
-      <Stack.Screen name="auth" />
-    </Stack>
+      <AuthProvider>
+        <Stack>
+          <Stack.Screen
+            name="(shop)"
+            options={{ title: "Shop", headerShown: false }}
+          />
+          <Stack.Screen
+            name="categories"
+            options={{ title: "Categories", headerShown: false }}
+          />
+          <Stack.Screen
+            name="product"
+            options={{ title: "Product", headerShown: true }}
+          />
+          <Stack.Screen
+            name="cart"
+            options={{
+              presentation: "modal",
+              title: "Shopping Cart",
+              // Override globally set option
+            }}
+          />
+          <Stack.Screen name="auth" />
+        </Stack>
+      </AuthProvider>
     </ToastProvider>
   );
 }
